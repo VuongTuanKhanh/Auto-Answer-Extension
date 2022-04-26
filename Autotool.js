1be815ceb1c249deae15e5d7be2c21029eeb2ceb
Archius Vương <khanhvuongtuan37@gmail.com>
2022-04-26 11:50:08 +0700

    Add all

---
+++ Autotool.js
@@ -0,0 +1,133 @@
+let results = {
+    "What does the command python manage.py migrate do in Python development?": ["Apply database migrations"],
+    "Which of the following is true regarding stocks and bonds": ["A mixed portfolio of stocks and bonds is always riskier than a pure stock portfolio", "It is possible that a mixed portfolio of stocks and bonds can have similar returns vs. a pure stock portfolio"],
+    "Is cartesian coordinate a machine learning algorithm?": ["false"],
+    "Which of the following is a good analogy for APR in the context of staking?": ["It is like interest rate", "It is like the interest rate / yield of a US federal bond", "It is like the interest rate"],
+    "Which of the following is a key benefit of smart contracts?": ["Minimal bias"],
+    "What is one advantage of deep learning over traditional machine learning methods?": ["Deep learning requires less labor-intensive feature engineering"],
+    "Is linear algebra a machine learning algorithm?": ["false"],
+    "Is the British Pound a fungible currency?": ["Yes"],
+    "Is the Chinese Yuan a fungible currency?": ["Yes"],
+    "Which of the following is NOT true for blockchain smart contracts?": ["They are centralized by nature"],
+    "When building a good machine learning model, having more training data is always better than having less training data": ["False"],
+    "Is Gaussian Mixture Model clustering a machine learning algorithm?": ["Yes, it is an unsupervised machine learning algorithm"],
+    "Which of the following does NOT have artificial intelligence (AI) embedded in it?": ["The Calculator App on your smartphone or laptop device", "The SIM card of your phone", "The native Messenger App on your smartphone device", "The light bulb of a remote-controlled lamp", "Smoke detector", "A hardware wallet holding Bitcoins"],
+    "In general, are small-cap stocks riskier than large-cap stocks?": ["Yes, bacause small-cap stocks tend to be more sensitive to perturbations in the broader market", "Yes", "Yes, bacause small-cap stocks tend to be high-beta", "Yes, bacause small-cap stocks tend to be high-beta", "Yes, bacause small-cap stocks tend to be more sensitive to perturbations in the broader market, bacause small-cap stocks tend to be high-beta"],
+    "In general, companies with higher P/E ratios have achieved their stock prices because:": ["Both of the above", "Investors expect the companies to achieve robust revenue growth in the future and are paying a premium for that", "Investors can be a bit over-paying in order to acquire the popular shares of those companies"],
+    "If a stock has low beta, it generally has:": ["Relative low risk to invest", "Low sensitivity to broader market fluctuations", "Low volatility", "Low risk", "Low market sensitivity", "Low volatility and low market sensitivity"],
+    "In general, which of the following does NOT characterize a high quality stock/company?": ["The company is always environmentally friendly", "The company is always in the energy storage or genomics sector", "The company is in the consumer cyclical business/sector"],
+    "What are example types of momentum as a key quant factor in the stock market?": ["Price momentum", "Earnings momentum"],
+    "Is Principle Component Analysis (PCA) a machine learning algorithm?": ["Yes, it is an unsupervised machine learning algorithm"],
+    "Do you need a minimal AVC balance to start staking?": ["No, you can stake as long as there is a non-zero balance in your Metamask wallet"],
+    "Is linear SVM a machine learning algorithm?": ["Yes, it is a supervised machine learning algorithm"],
+    "What is the typical function of index.html in Python/Django web development?": ["Frontend UI"],
+    "What is the typical function of models.py in Python/Django web development?": ["Database table definitions"],
+    "Which of the following is NOT covered in settings.py in the context of Django web development?": ["Importing the JQuery library"],
+    "In order to test the performance of a machine learning model, you need:": ["Test data"],
+    "Which of the following does NOT use machine learning?": ["The Calculator app on your smartphone device", "The remote controller of your TV", "The repeat-one feature of Spotify", "Heating some dishes in a microwave", "A Xerox printer", "Minting a new cryptocurrency on the Binance Smart Chain (BSC)", "Charging a gasoline-power vehicle at your local gas station", "A ferry boat from Seattle to Vancouver", "Minting a new cryptocurrency on the Binance Smart … gasoline-power vehicle at your local gas station"],
+    "What is one difference between adjusted close price and raw close price?": ["The former typically accounts for stock split events", "The former is more frequently used for trading strategy back-testing than the latter"],
+    "If you are staking 500 AVC at 7.5% APR, what will be your daily AVC reward?": ["500 x 7.5% / 365"],
+    "If you are staking 500 AVC at 7.5% APR, what will be your annual AVC reward?": ["500 x 7.5%"],
+    "Is linear algebra a machine learning algorithm?": ["No"],
+    "Is the Korean Won a fungible currency?": ["Yes"],
+    "If you are staking 500 AVC at 7.5% APR, what will be your monthly AVC reward?": ["500 x 7.5% / 12"],
+    "Are non-fungible tokens part of the ERC20 family?": ["No"],
+    "Is the US Dollar a fungible currency?": ["Yes"],
+    "Which of the following is NOT true about test data in machine learning tasks?": ["They are a clone of the training data", "The model performance will always be higher on test data than on training data"],
+    "Is decision tree a machine learning algorithm?": ["Yes, it is a supervised machine learning algorithm"],
+    "Is Pythagorean Theorem a machine learning algorithm?": ["No"],
+    "Are fungible cryptocurrencies part of the ERC20 family?": ["It depends. There are no definite yes/no answers"],
+    "In general, companies that have higher P/E ratios also have:": ["Higher growth expected by investors and financial news media", "Higher growth in expected future cash flows", "Higher growth rates"],
+    "If you are staking 500 AVC at 7.5% APR, what will be your weekly AVC reward?": ["500 x 7.5% / 365 x 7"],
+    "Which of the following is true for Alpha Vantage Coin (AVC)?": ["It is a dual-purpose utility & governance token to empower all financial market participants in the world", "You can mine the coin without writing any software code/scripts", "It can be mined without any specialized mining hardware or software", "It is on the Ethereum blockchain, following the ERC20 standard", "By holding AVC, one can vote for the governance roadmap of the project", "By holding AVC, one can unlock a high-volume API key for the Alpha Vantage APIs", "By holding AVC, one can vote for the governance roadmap of the AVC project"],
+    "Which of the following is NOT true for AVC staking?": ["It is not possible to stake AVC", "The staking reward is distributed once every quarter (3 months)"],
+    "Which of the following is true about test data in machine learning tasks?": ["They are used to measure the performance of the trained model", "They can be used to verify the robustness of the trained model"],
+    "Which of the following is true for AVC staking?": ["It can be enabled in your Alpha Tournament dashboard with just one mouse click - no coding required", "You can earn additional AVC tokens through staking", "The staking reward is distributed daily"],
+    "Is linear regression a good machine learning model for predicting stock prices?": ["No, because stock price movements are generally non-linear"],
+    "Which of the following is true about training data in machine learning tasks?": ["They are labeled with answers in supervised learning settings", "They are labeled with input (X) and output (Y) for the model to learn"],
+    "Which of the following is an application of smart contracts?": ["An ERC20 token such as Alpha Vantage Coin (AVC)", "A decentralized exchange (DEX) such as Uniswap", "An ERC20 token that has a low trading volume on Uniswap", "A web3 decentralized application (dapp) such as the Alpha Tournament", "An ERC20 token on the Ethereum blockchain", "An ERC20…he cryptocurrency USD Coin (USDC)", "Metamask wallet", "The cryptocurrency USD Coin (USDC)"],
+    "Do you need real-time data for ALL machine learning tasks?": ["No"],
+    "Is cartesian coordinate a machine learning algorithm?": ["No"],
+    "Do you need training data for machine learning tasks?": ["It depends on the use case"],
+    "What does Django mean in the context of Python web development?": ["It is a web development framework written in Python"],
+    "What is the typical function of runtime.txt files in Python/Django web development?": ["Python runtime version definitions"],
+    "What is the typical function of urls.py in Python/Django web development?": ["Set up URL routing"],
+    "What is the typical function of db.sqlite3 in Python/Django web development?": ["Database configurations"],
+    "What is the typical function of files in the .css format in Python/Django web development?": ["Frontend rendering"],
+    "What is the typical function of requirements.txt in Django web development?": ["Listing the required dependencies and libraries"],
+    "What is the typical function of settings.py in Django web development?": ["App configurations"],
+    "What is the typical function of views.py in Python/Django web development?": ["Backend logic"],
+    "What is the typical function of the JQuery Library in web development?": ["Frontend logic"],
+    "What does AJAX mean in the context of Python web development?": ["Asynchronous JavaScript and XML"],
+    "What does the command pip install do in Python development?": ["Install Python libraries"],
+    "Which of the following is NOT a key attribute of ERC20 tokens?": ["They are all traded on Coinbase"],
+    "Is deep learning always better than other machine learning algorithms?": ["It depends on the use case"],
+    "What is the relationship between machine learning and deep learning?": ["Deep learning is a special class of machine learning algorithms"],
+    "Which of the following is NOT a key benefit of smart contracts?": ["Improving the diversity and inclusion in the US labor market"],
+    "Tesla (TSLA) had a five-for-one stock split in August 2020. Which of the following is TRUE?": ["TSLA shareholders had more shares after the split"],
+    "Apple (AAPL) had a four-for-one stock split in August 2020. Which of the following is TRUE?": ["AAPL shareholders had more shares after the split"],
+    "Which of the following will NOT happen when a stock goes through a stock split event?": ["It will be delisted from the NASDAQ Stock Exchange"],
+    "Which of the following is a key attribute of ERC20 tokens": ["Decentralization"],
+    "Which of the following is NOT true for ERC20 tokens?": ["They are centralized by nature"],
+    "Which of the following is true about ANY machine learning algorithm with good performance?": ["It will definitely perform well on the training dataset"],
+    "In order to build a deep learning model, you need:": ["A computing resource equipped with either CPU or GPU", "Training data", "A mathematical way for the model to converge"],
+    "Which of the following is a key attribute of smart contracts?": ["Decentralization"],
+    "Which of the following is NOT true for Python/Django?": ["It can be used for developing iOS applications", "It can be used for developing video games powered by Unity"],
+    "What is the relationship between machine learning and artificial intelligence?": ["Machine learning is an important tool to achieve general artificial intelligence"],
+    "Is the XXX_CURRENCY (US Dollar, Korean Won, Japanese Yen, British Pound, Euro, etc) a fungible token?": ["Yes"],
+    "Are ERC20 tokens fungible?": ["Yes"],
+    "Is Bitcoin a fungible token?": ["Yes"],
+    "Which of the following is NOT true regarding stocks and bonds": ["High-beta stocks are generally considered safe haven assets"],
+    "What makes the Webull investing platform special?": ["Webull allows you to invest with zero commissions, no account minimums & in-depth analysis tools"],
+    "Are commodities such as Gold and Silver fungible assets?": ["Yes"],
+    "Are commodities such as Copper fungible assets?": ["Yes"],
+    "Which of the following is true about PyTorch?": ["The Py part in PyTorch means Python", "It is open-sourced"],
+    "Which of the following is NOT true about PyTorch?": ["It is a for-profit project", "It is the building block of blockchain technologies"],
+    "Do you need central processing units (CPU) for deep learning tasks?": ["It depends on the use case"],
+    "Do you need graphical processing units (GPU) for deep learning tasks?": ["It depends on the use case"],
+    "Does deep learning always deliver better performance than other machine learning algorithms?": ["It depends on the use case"],
+    "Is polar coordinate a machine learning algorithm?": ["No"],
+    "When building a good machine learning model, having more training data is always better than having less training data": ["No", "False"],
+    "Is Bitcoin a fungible currency?": ["Yes"],
+    "Is the Japanese Yen a fungible currency?": ["No"],
+    "What is Webull?": ["Webull is an investing platform with zero commissions, no account minimums & in-depth analysis tools to advance your portfolio"],
+    "If you are staking 5000 AVC at 7.5% APR, what will be your daily AVC reward?": ["5000 x 7.5% / 365"],
+    "What can you earn for signing up & funding your account with Webull?": ["5 free stocks added to your portfolio worth at least $25", "Earn 5 free stocks", "2 free stocks each worth $3 - $300 after you sign up + 3 free stocks each worth $7 - $3000 after you fund your account"],
+    "Is k-means clustering a machine learning algorithm?": ["Yes, it is an unsupervised machine learning algorithm"],
+    "Is Naive Bayes a machine learning algorithm?": ["Yes, it is a supervised machine learning algorithm"],
+    "How much commission does Webull charge for equity trades?": ["$0 / trade"],
+    "What is the current Webull signup bonus?": ["Get 5 free stocks when you open and fund a new account"],
+    "What can you earn for downloading Webull’s investing app?": ["2 free stocks when you sign up and 3 free stocks when you fund your account"],
+    "Which of the following is NOT a key attribute of smart contracts?": ["Always open-sourced"],
+    "Is the Euro a fungible currency?": ["Yes"]
+}
+
+let questions = document.body.innerText.split("\n").filter(element => element.includes("Question ")).map(question => question.substring(12));
+
+// let answer = $("label").toArray();
+let answer = Array.from(document.querySelectorAll("label"))
+
+for (let i = 0; i < 3; ++i) {
+    let curr_question = questions[i];
+    let answer_result = results[curr_question];
+
+    if (answer_result == undefined) {
+        alert("Please notice that the question " + curr_question + " is not in the dictionary");
+        continue;
+    }
+
+    let flag = false;
+    for (let j = 0; j < 4; ++j) {
+        if (j == 3) {
+            if (!flag) {
+                alert("Please notice that the question " + curr_question + " is not in the dictionary");
+            }
+            break;
+        }
+        if (answer_result.includes(answer[i * 3 + j].innerText)) {
+            answer[i * 3 + j].click();
+            flag = true;
+            continue;
+        }
+    }
+}
